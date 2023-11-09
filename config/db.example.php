bdf6592c535533d3ff9673b9540336cca167e975
Lucas Veras <lucas.veras@tecnisys.com.br>
2023-11-09 18:03:58 -0300

    :white_check_mark: Testando gitignore

--- config/db.example.php
+++ config/db.example.php
@@ -2,8 +2,8 @@
 #=================================================
 # PARAMETROS DO BANCO DE DADOS - MYSQL + PDO
 #=================================================
-const HOST_DB = 'localhost';
-const PORT = '3306';
-const DBNAME = 'dbdesaparecido';
-const USER_DB = 'root';
-const PASSWORD_DB = 'lu010811';
+const HOST_DB = '';
+const PORT = '';
+const DBNAME = '';
+const USER_DB = '';
+const PASSWORD_DB = '';
